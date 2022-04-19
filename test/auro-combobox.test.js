60546478112b0e759e31127b82f6aa8ea453594f
Jason Baker <jason@capsule42.com>
2022-04-19 10:38:55 -0700

    refactor(tests): adjustments for test coverage

--- test/auro-combobox.test.js
+++ test/auro-combobox.test.js
@@ -1,27 +1,18 @@
-// import { fixture, html, expect } from '@open-wc/testing';
-// import '../src/auro-combobox.js';
+import { fixture, html, expect } from '@open-wc/testing';
+import '../src/auro-combobox.js';
 
-// describe('auro-combobox', () => {
-//   it('sets the CSS class on auro-combobox > div element', async () => {
-//     const el = await fixture(html`
-//       <auro-combobox cssclass="testClass"></auro-combobox>
-//     `);
+describe('auro-combobox', () => {
+  // it('auro-combobox is accessible', async () => {
+  //   const el = await fixture(html`
+  //     <auro-combobox></auro-combobox>
+  //   `);
 
-//     const div = el.shadowRoot.querySelector('div');
-//     expect(div.className).to.equal('testClass');
-//   });
+  //   await expect(el).to.be.accessible();
+  // });
 
-//   it('auro-combobox is accessible', async () => {
-//     const el = await fixture(html`
-//       <auro-combobox cssclass="testClass"></auro-combobox>
-//     `);
+  it('auro-combobox custom element is defined', async () => {
+    const el = await !!customElements.get("auro-combobox");
 
-//     await expect(el).to.be.accessible();
-//   });
-
-//   it('auro-combobox custom element is defined', async () => {
-//     const el = await !!customElements.get("auro-combobox");
-
-//     await expect(el).to.be.true;
-//   });
-// });
+    await expect(el).to.be.true;
+  });
+});
