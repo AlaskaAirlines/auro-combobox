1f97289b00df51065a4ff86a91cb231954fb926c
Dale Sande <dale.sande@gmail.com>
2022-11-07 15:24:32 -0800

    docs(demo): address post review content updates
    Changes to be committed:
    modified:   apiExamples/dynamicMenu.html
    modified:   apiExamples/dynamicMenu.js
    modified:   demo/api.md
    modified:   docs/partials/api.md

--- apiExamples/dynamicMenu.js
+++ apiExamples/dynamicMenu.js
@@ -1,50 +1,52 @@
+// Javascript example file
+// -----------------------
 import { DynamicData } from './dynamicMenuDataApi';
 
 // Resets the root menu
 function resetMenu(root) {
-    while (root.firstChild) {
-        root.removeChild(root.firstChild);
-    }
+  while (root.firstChild) {
+    root.removeChild(root.firstChild);
+  }
 }
 
 // Generates HTML for menu and submenus using country & city data from an external API
 function generateHtml(data) {
-    const initialMenu = document.querySelector('#initMenu');
+  const initialMenu = document.querySelector('#initMenu');
 
-    resetMenu(initialMenu);
+  resetMenu(initialMenu);
 
-    for (let index = 0; index < data.length; index++) {
-        let country = data[index]['country'];
-        let cities = data[index]['cities'];
+  for (let index = 0; index < data.length; index++) {
+    let country = data[index]['country'];
+    let cities = data[index]['cities'];
 
-        generateMenuOptionHtml(initialMenu, country, country);
+    generateMenuOptionHtml(initialMenu, country, country);
 
-        for (let indexB = 0; indexB < cities.length; indexB++) {
-            let subMenu = document.createElement('auro-menu');
+    for (let indexB = 0; indexB < cities.length; indexB++) {
+      let subMenu = document.createElement('auro-menu');
 
-            generateMenuOptionHtml(subMenu, cities[indexB], cities[indexB]);
-    
-            initialMenu.appendChild(subMenu);
-        }
-    };
+      generateMenuOptionHtml(subMenu, cities[indexB], cities[indexB]);
+
+      initialMenu.appendChild(subMenu);
+    }
+  };
 }
 
 // Helper function that generates HTML for menuoptions
 function generateMenuOptionHtml(menu, label, value) {
-    let option = document.createElement('auro-menuoption');
+  let option = document.createElement('auro-menuoption');
 
-    option.value = value;
-    option.innerHTML = label;
+  option.value = value;
+  option.innerHTML = label;
 
-    menu.appendChild(option);
+  menu.appendChild(option);
 }
 
 // Main function that runs all JS to create example
 export async function populateCombobox(elem) {
-    const dynamicData = new DynamicData();
+  const dynamicData = new DynamicData();
 
-    let data = await dynamicData.getData();
-    data = dynamicData.filterData(data, elem.value);
+  let data = await dynamicData.getData();
+  data = dynamicData.filterData(data, elem.value);
 
-    generateHtml(data);
+  generateHtml(data);
 }
--- apiExamples/dynamicMenuDataApi.js
+++ apiExamples/dynamicMenuDataApi.js
@@ -1,31 +1,31 @@
 class DynamicData {
-    getData() {
-        const response = [
-            {iso2: 'AR', iso3: 'ARG', country: 'Argentina', cities: ['Germania']}, 
-            {iso2: 'CA', iso3: 'CAN', country: 'Canada', cities: ['Saint-Germain-de-Grantham']}, 
-            {iso2: 'FR', iso3: 'FRA', country: 'France', cities: ['Chatel-Saint-Germain', 'Domgermain', 'Germaine', 'Germainville', 'Germenay']}, 
-            {iso2: 'DE', iso3: 'DEU', country: 'Germany', cities: ['Algermissen', 'Angermunde', 'Germering', 'Hilgermissen', 'Tangermunde']}, 
-            {iso2: 'IR', iso3: 'IRN', country: 'Iran', cities: ['Germi']}
-        ];
-        
-        return response;
-    }
-    
-    filterData(data, value) {
-        const filteredData = [];
-    
-        for (let index = 0; index < data.length; index ++) {
-            let countryName = data[index]['country'].toLowerCase();
-    
-            data[index]['cities'] = data[index]['cities'].filter(city => city.toLowerCase().includes(value.toLowerCase()));
-    
-            if (value && (countryName.includes(value.toLowerCase()) || data[index]['cities'].length > 0)) {
-                filteredData.push(data[index]);
-            }
-        };
-    
-        return filteredData;
-    }
+  getData() {
+    const response = [
+      {iso2: 'AR', iso3: 'ARG', country: 'Argentina', cities: ['Germania']},
+      {iso2: 'CA', iso3: 'CAN', country: 'Canada', cities: ['Saint-Germain-de-Grantham']},
+      {iso2: 'FR', iso3: 'FRA', country: 'France', cities: ['Chatel-Saint-Germain', 'Domgermain', 'Germaine', 'Germainville', 'Germenay']},
+      {iso2: 'DE', iso3: 'DEU', country: 'Germany', cities: ['Algermissen', 'Angermunde', 'Germering', 'Hilgermissen', 'Tangermunde']},
+      {iso2: 'IR', iso3: 'IRN', country: 'Iran', cities: ['Germi']}
+    ];
+
+    return response;
+  }
+
+  filterData(data, value) {
+    const filteredData = [];
+
+    for (let index = 0; index < data.length; index ++) {
+      let countryName = data[index]['country'].toLowerCase();
+
+      data[index]['cities'] = data[index]['cities'].filter(city => city.toLowerCase().includes(value.toLowerCase()));
+
+      if (value && (countryName.includes(value.toLowerCase()) || data[index]['cities'].length > 0)) {
+        filteredData.push(data[index]);
+      }
+    };
+
+    return filteredData;
+  }
 }
-  
+
 export { DynamicData }
