30777b0e41eb67577168f4598ff5e8c8adeae8d7
Jordan Jones <jmjones2463@gmail.com>
2023-05-11 10:14:54 -0700

    docs: add explanation comments to example files #140

--- demo/api.js
+++ demo/api.js
@@ -3,6 +3,7 @@ import { valueExample } from '../apiExamples/value';
 import { focusExample } from '../apiExamples/focus';
 
 export function initComboboxApiExamples() {
+  // setTimeout handles issue where content is sometimes loaded after the functions get called
   setTimeout(() => {
     dynamicMenuExample();
     valueExample();
--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,7 @@ import { persistentExample } from '../apiExamples/persistent';
 import { swapValueExample } from '../apiExamples/swapValue';
 
 export function initComboboxIndexExamples() {
+  // setTimeout handles issue where content is sometimes loaded after the functions get called
   setTimeout(() => {
     persistentExample();
     swapValueExample();
