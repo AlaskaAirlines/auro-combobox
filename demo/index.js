37337af8189b0c9436b1ee2b1bb6d34b895f7bf4
Jordan Jones <jmjones2463@gmail.com>
2023-05-11 10:43:48 -0700

    perf(examples): refactor initialization of examples #140

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,18 @@
 import { persistentExample } from '../apiExamples/persistent';
 import { swapValueExample } from '../apiExamples/swapValue';
 
-export function initComboboxIndexExamples() {
-  // setTimeout handles issue where content is sometimes loaded after the functions get called
-  setTimeout(() => {
+export function initComboboxIndexExamples(initCount) {
+  initCount = initCount || 0;
+
+  try {
     persistentExample();
     swapValueExample();
-  }, 100);
+  } catch {
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initComboboxIndexExamples(initCount + 1);
+      }, 100);
+    }
+  }
 }
