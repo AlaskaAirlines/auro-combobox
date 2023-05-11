0f0e7bca51b443a0f06cba854ef141b27d7716af
Jordan Jones <jmjones2463@gmail.com>
2023-05-11 10:43:48 -0700

    perf(examples): refactor initialization of examples #140

--- demo/api.js
+++ demo/api.js
@@ -2,11 +2,19 @@ import { dynamicMenuExample } from '../apiExamples/dynamicMenu';
 import { valueExample } from '../apiExamples/value';
 import { focusExample } from '../apiExamples/focus';
 
-export function initComboboxApiExamples() {
-  // setTimeout handles issue where content is sometimes loaded after the functions get called
-  setTimeout(() => {
+export function initComboboxApiExamples(initCount) {
+  initCount = initCount || 0;
+
+  try {
     dynamicMenuExample();
     valueExample();
     focusExample();
-  }, 100);
+  } catch {
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initComboboxApiExamples(initCount + 1);
+      }, 100);
+    }
+  }
 }
