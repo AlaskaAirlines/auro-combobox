02e42cef6c78ad11e2a36b1382386873e8a12910
Jason Baker <jason@capsule42.com>
2023-05-17 14:59:03 -0700

    perf(examples): modify the bundles created for the examples when running the build #144

---
+++ scripts/prepExampleFiles.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+
+function removeExport(path, type) {
+  fs.readFile(path, type, function(err, data) {
+    const exportPos = data.indexOf('export')
+    const exampleScript = data.substring(0, exportPos);
+    const writer = fs.createWriteStream(path);
+
+    writer.write(exampleScript);
+  });
+}
+
+removeExport('./demo/index.min.js', 'utf8');
+removeExport('./demo/api.min.js', 'utf8');
