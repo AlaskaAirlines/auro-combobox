0c66b3ac31bd780512a488f2a4da35a4b8277a48
Dale Sande <dale.sande@gmail.com>
2023-05-24 13:41:55 -0700

    build: update rollup to support examples
    Changes to be committed:
    modified:   rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,24 +3,44 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 
-const production = !process.env.ROLLUP_WATCH,
+const production = !process.env.ROLLUP_WATCH;
 
-  modernConfig = {
-    input: {
-      'auro-combobox__bundled': './index.js',
-    },
-    output: {
-      format: 'esm',
-      dir: 'dist/'
-    },
-    plugins: [
-      nodeResolve(),
-      !production &&
+const modernConfig = {
+  input: {
+    ['auro-combobox__bundled']: './index.js',
+  },
+  output: {
+    format: 'esm',
+    dir: 'dist/'
+  },
+  plugins: [
+    nodeResolve(),
+    !production &&
       serve({
         open: true,
         openPage: '/docs/'
       })
-    ]
-  };
+  ]
+};
 
-export default [modernConfig];
+const indexExamplesConfig = {
+  input: {
+    ['index.min']: './demo/index.js',
+  },
+  output: {
+    format: 'esm',
+    dir: 'demo/'
+  }
+};
+
+const apiExamplesConfig = {
+  input: {
+    ['api.min']: './demo/api.js',
+  },
+  output: {
+    format: 'esm',
+    dir: 'demo/'
+  }
+};
+
+export default [modernConfig, indexExamplesConfig, apiExamplesConfig];
