c96d4648eb48e5e02ae3eda6caad26e8278bd0f1
Dale Sande <dale.sande@gmail.com>
2024-02-09 20:49:32 -0800

    perf: update dependencies to support SSR
    Changes to be committed:
    modified:   package-lock.json
    modified:   package.json
    modified:   packageScripts/postinstall.mjs

--- packageScripts/postinstall.mjs
+++ packageScripts/postinstall.mjs
@@ -23,6 +23,11 @@ console.log(chalk.hex('#f26135')(`
         Thanks for installing the latest version
         of `) + chalk.hex('#ffd200').bold(`${pjson.name} v${pjson.version}.`) + chalk.hex('#f26135')(`
 
+          IMPORTANT: Auro Combobox requires
+        @alaskaairux/icons ${pjson.dependencies['@alaskaairux/icons']} installed.
+
+           Be sure to check your dependencies.
+
 ╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
 `)
 );
