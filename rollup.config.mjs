f37042c95401007c6cc4e4306069a71ad83d495e
Jason Baker <jason@capsule42.com>
2024-11-05 11:48:34 -0700

    perf: improve rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -30,7 +30,8 @@ const indexExamplesConfig = {
   output: {
     format: 'esm',
     dir: 'demo/'
-  }
+  },
+  plugins: [nodeResolve()]
 };
 
 const apiExamplesConfig = {
@@ -40,7 +41,8 @@ const apiExamplesConfig = {
   output: {
     format: 'esm',
     dir: 'demo/'
-  }
+  },
+  plugins: [nodeResolve()]
 };
 
 export default [modernConfig, indexExamplesConfig, apiExamplesConfig];
